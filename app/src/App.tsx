40b1c4a1836d893f5ce035f101662482ac304036
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-09-22 13:58:14 -0500

    Add navbar and update routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,38 +1,19 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Button} from "@mui/material";
-import axios from "axios";
-
-
+import ResponsiveAppBar from "./components/layout/ResponsiveAppBar";
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
-  const handleOnClick=()=>{
-    console.log("click")
-    axios.get("http://localhost:8000/test/quizzes/2").then(res=>{
-          console.log(res);
-        }
-    )
-  }
-
-  useEffect(()=>{
-      axios.get("http://localhost:8000/log/howdy").then(res=>{
-              console.log(res);
-          }
-      )
-  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button onClick = {handleOnClick}>
-          Learn React
-        </Button>
-      </header>
+      <ResponsiveAppBar/>
+        <Routes>
+            <Route path={"/quizzes"} element={<h1>QUIZZES</h1>}/>
+            <Route path={"/play"} element={<h1>PLAY</h1>}/>
+            <Route path={"/login"} element={<h1>LOGIN</h1>}/>
+            <Route path={"/logout"} element={<h1>LOGOUT</h1>}/>
+        </Routes>
     </div>
   );
 }
---
+++ app/src/components/layout/Header.jsx
@@ -0,0 +1,61 @@
+// import React from 'react';
+// // import Link from '@emotion/styled/macro';
+// import Link from '@mui/material/Link';
+// import {AppBar} from "@mui/material";
+// import {Toolbar} from "@mui/material";
+// import { css } from '@emotion/react';
+// // import {makeStyles} from "@mui/styles";
+//
+// import { createTheme } from '@mui/material/styles';
+// import { purple } from '@mui/material/colors';
+//
+// // const theme = createTheme({
+// //     palette: {
+// //         primary: {
+// //             main: purple[500],
+// //         },
+// //         secondary: {
+// //             main: '#f44336',
+// //         },
+// //     },
+// // });
+//
+// const navigationLinks = [
+//     {name: "Home", href: "/"},
+//     {name: "Pitch Recognition", href: "/pitch-recognition"},
+//     {name: "Transposition", href:"/transposition" }
+// ]
+//
+// // const useStyles = makeStyles((theme)=>({
+// //     link: {
+// //         marginRight : "20px"
+// //     }
+// // }));
+//
+// // sx={{color : '#7A00E4'}}
+//
+// //Tutorial: https://www.youtube.com/watch?v=gCEoLdufFLU
+//
+// const Header = () => {
+//     return (
+//         <div>
+//             <AppBar sx={{backgroundColor: '#7A00E4'}} position={"sticky"}>
+//                 <Toolbar > Valvio
+//                     {navigationLinks.map((item, i)=>{
+//                         return <Link underline = "none"
+//                                      color="textPrimary"
+//                                      sx={{
+//                                          margin : "14px"
+//                                      }}
+//                                      key = {i}
+//                             >{item.name}</Link>
+//                     })}
+//                 </Toolbar>
+//             </AppBar>
+//
+//         </div>
+//     );
+// };
+//
+// export default Header;
+
