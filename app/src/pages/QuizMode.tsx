630f69621605855afb8779f173361badac488a7e
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-09-26 13:49:01 -0500

    Add components for quiz mode

---
+++ app/src/pages/QuizMode.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MetricsCard from "../components/ui/MetricsCard";
+import QuizModeCard from "../components/ui/QuizModeCard";
+
+const QuizMode = () => {
+    return (
+        <div>
+            <MetricsCard/>
+            <QuizModeCard/>
+        </div>
+    );
+};
+
+export default QuizMode;
