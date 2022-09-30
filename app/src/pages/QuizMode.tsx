5a3728b7a59ef9849c5d29fd57319b2c4657f495
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-09-30 15:23:38 -0500

    Update QuizMode.tsx

--- app/src/pages/QuizMode.tsx
+++ app/src/pages/QuizMode.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import MetricsCard from "../components/ui/MetricsCard";
 import QuizModeCard from "../components/ui/QuizModeCard";
+import {useCreateQuizAttemptMutation} from "../features/quizData/quiz-api";
 
 const QuizMode = () => {
+    //Start Quiz
+    //Get Quizzes AND put into state
+    //When both are done, show first pitch to user
+    //After user submits then send a request off AND update state
+    //Check if the pitchesCompleted >= quizLength. If so, reveal a different component AND send a request AND update state
+
     return (
         <div>
             <MetricsCard/>
