0bb3f734249760248406f5b7c5e3b02b5337c4cd
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-09-30 15:18:03 -0500

    Create QuizItemPersonalMetrics.tsx

---
+++ app/src/components/ui/QuizItemPersonalMetrics.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import {LinearProgress} from "@mui/material";
+import {QuizAttempt} from "../../models/QuizAttempt";
+
+const QuizItemPersonalMetrics: React.FC<{highScore: QuizAttempt | null}> = (props) => {
+
+
+    return (
+        <div>
+            {props.highScore ? <h6>HIGHSCORE</h6> : <h6>None</h6>
+            }
+            <i>Loading Personal Metrics</i>
+            <LinearProgress color="inherit" />
+        </div>
+    );
+};
+
+export default QuizItemPersonalMetrics;
