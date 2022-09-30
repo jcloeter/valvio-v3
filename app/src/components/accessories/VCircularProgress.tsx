b605314fe4498c88b3ebae209783c384a7f36154
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-09-30 15:15:41 -0500

    Create VCircularProgress.tsx

---
+++ app/src/components/accessories/VCircularProgress.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CircularProgress from "@mui/material/CircularProgress";
+
+const VCircularProgress = () => {
+    return (
+        <div>
+            <CircularProgress color = "primary"/>
+        </div>
+    );
+};
+
+export default VCircularProgress;
