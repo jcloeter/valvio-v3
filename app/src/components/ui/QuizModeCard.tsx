ac1577d67acff3c83896d93c0553ddf12168cf80
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-03 15:51:39 -0500

    Add logic for game mode

--- app/src/components/ui/QuizModeCard.tsx
+++ app/src/components/ui/QuizModeCard.tsx
@@ -3,15 +3,56 @@ import LightGreyCard from "./LightGreyCard";
 import Button from "@mui/material/Button";
 import ValveGroup from "./ValveGroup";
 import styles from "./QuizModeCard.module.css"
+import {useSelector} from "react-redux";
+import {RootState} from "../../features/store";
+import {Pitch} from "../../models/Pitch";
+import {Pitches} from "../../models/Pitches";
+import QuizSlice, {submitWrongAnswer} from "../../features/quizData/quizSlice";
+import {convertPitchInstrumentIdToImageId} from "./convertPitchInstrumentIdToImageId";
+import {useAppDispatch} from "../../features/hooks";
+import {useCreateQuizPitchAttemptMutation} from "../../features/quizData/quiz-api";
+
 
 
 const QuizModeCard = () => {
+    // const pitches = useSelector<RootState, Pitch>((state) => state.quizAttemptSlice.extendedPitches);
+    const currentPitchIndex = useSelector<RootState, number>((state) => state.quizAttemptSlice.currentPitchIndex);
+    // @ts-ignore
+    const quizAttemptSlice = useSelector<RootState, QuizSlice>((state) => state.quizAttemptSlice);
+    const dispatch = useAppDispatch();
+    const [createQuizPitchAttemptMutation, {data, isLoading, isError}] = useCreateQuizPitchAttemptMutation();
+
+    console.log(quizAttemptSlice.extendedPitches[currentPitchIndex][0]["position"]);
+
+    console.log(quizAttemptSlice.extendedPitches[0][0]["id"]);
+    const pitchId = quizAttemptSlice.extendedPitches[0][0]["id"];
+    const imageId = convertPitchInstrumentIdToImageId(pitchId);
+    const imageUrl = `https://valvio-data-bucket.s3.us-east-2.amazonaws.com/valvio_pitches/${imageId}_treble.png`;
+
+    const handleSubmitButton = () => {
+        console.log("You're probably wrong but ok!");
+        dispatch(submitWrongAnswer());
+
+        //TODO: Make sure that the data for transpositions is getting included
+        //Todo: this is not real data:
+
+        const body = {
+            isCorrect: true,
+            userInput: 0,
+            quizPitchId: 10,
+            quizAttemptId: 300,
+        };
+        createQuizPitchAttemptMutation({userId: 7, body});
+    }
+
     return (
         <LightGreyCard>
             <>
-                <img className = {styles["pitch-img"]} src="https://valvio-data-bucket.s3.us-east-2.amazonaws.com/valvio_pitches/cna5_treble.png" alt="pitch"/>
+                <img className = {styles["pitch-img"]} src={imageUrl} alt="pitch"/>
                 <ValveGroup/>
-                <Button variant="contained" size ="large" sx={{
+                <Button
+                    onClick ={handleSubmitButton}
+                    variant="contained" size ="large" sx={{
                     width: '70%',
                 }} >Submit</Button>
             </>
@@ -20,3 +61,5 @@ const QuizModeCard = () => {
 };
 
 export default QuizModeCard;
+
+// "https://valvio-data-bucket.s3.us-east-2.amazonaws.com/valvio_pitches/cna5_treble.png"
--- app/src/features/quizData/quizSlice.tsx
+++ app/src/features/quizData/quizSlice.tsx
@@ -1,34 +1,72 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {quizApi} from "./quiz-api";
+import {Pitch} from "../../models/Pitch";
+import {randomizeAndExtendPitchArray} from "../../helper/randomizeAndExtendPitchArray";
+import {convertPitchInstrumentIdToImageId} from "../../components/ui/convertPitchInstrumentIdToImageId";
 
 //Todo: Add createQuizPitchAttempt, PATCH quizAttempt
 //Todo: update slices before sending the request!!!
 
+interface QuizSlice {
+    quizId: number | null,
+    userid: number | null
+    currentScore: number,
+    correctPitchAttempts: number,
+    incorrectPitchAttempts: number,
+    uniquePitches: Pitch[],
+    extendedPitches: Pitch[],
+    isTransposition: boolean,
+    transpositionInterval: number,
+    startTime: number | null,
+    endTime: number | null,
+    quizStatus:  "exited" | "in progress" | "complete" | "error" | "failed" | "uninitialized" ;
+    quizLength: number | null,
+    currentPitchIndex: number,
+}
 
-const initialState = {
-    quizId: 0,
-    userid: 7,
+
+const initialState: QuizSlice= {
+    quizId: null,
+    userid: null,
     currentScore: 0,
     correctPitchAttempts: 0,
     incorrectPitchAttempts: 0,
-    pitches: "no pitches",
+    uniquePitches: [],
+    extendedPitches: [],
+    isTransposition: false,
+    transpositionInterval: 0,
+    startTime: null,
+    endTime: null,
+    quizStatus: "uninitialized",
+    quizLength: null,
+    currentPitchIndex: 0,
 };
 
 export const quizSlice = createSlice({
     name: "current-quiz-slice",
     initialState : initialState,
     reducers: {
-        setPitches: (state, pitchesArr)=>{
-            state.pitches = "PITCHES";
-        },
         submitWrongAnswer: (state)=>{
             state.incorrectPitchAttempts += 1
         },
         submitCorrectAnswer: (state)=>{
             state.correctPitchAttempts += 1;
         },
+        setStartTime: (state) =>{
+            state.startTime = Date.now();
+        },
         viewTotalScore: ()=>{}
+    },
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(quizApi.endpoints.getPitchesByQuizId.matchFulfilled, (state, action)=> {
+                state.uniquePitches = action.payload.pitches;
+                state.extendedPitches = randomizeAndExtendPitchArray(action.payload.pitches, action.payload.quizLength);
+                state.isTransposition = action.payload.isTransposition;
+                state.quizId = action.payload.quizId;
+            })
     }
 });
 
 export default quizSlice.reducer;
-export const { setPitches, submitWrongAnswer, submitCorrectAnswer } = quizSlice.actions;
+export const { submitWrongAnswer, submitCorrectAnswer } = quizSlice.actions;
