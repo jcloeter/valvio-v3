1bdd98c8e74150b711051c5096daf0b7514d2448
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-05 08:17:47 -0500

    Add interactive valves

--- app/src/components/ui/ValveButton.tsx
+++ app/src/components/ui/ValveButton.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './ValveButton.module.css';
 
-const ValveButton = () => {
+const ValveButton: React.FC<{ valveNumber: string; keyBoardKey: string; onValveChange: (isPressed: boolean) => void }> = (props:{valveNumber: string, keyBoardKey: string, onValveChange: (isPressed: boolean)=>void}) => {
+    const [isPressed, setIsPressed] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('keydown', (event) => {
+            if (event.key === props.keyBoardKey){
+                console.log("Pressing down " + props.valveNumber);
+                setIsPressed((isPressed) => {
+                    props.onValveChange(!isPressed);
+                    return !isPressed;
+                });
+            }
+        });
+    }, []);
+
+    //Start here: Must fix bugs in the valve up/down logic
+
+    const handleValvePress = () => {
+        setIsPressed((isPressed) => {
+            props.onValveChange(!isPressed);
+            return !isPressed;
+        });
+    }
+
     return (
         <div>
-            <input type="checkbox" className = {styles["btn-valve"]}/>
+            <input type="checkbox"
+                   className = {styles["btn-valve"]}
+                   onChange={handleValvePress}
+                   checked={isPressed}
+            />
         </div>
     );
 };
