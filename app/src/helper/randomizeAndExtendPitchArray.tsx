3bd0e8ef0a20bf9a19c0681a14604d73bd3a2bcc
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-05 08:17:33 -0500

    Update pitch algo to randomize notes

--- app/src/helper/randomizeAndExtendPitchArray.tsx
+++ app/src/helper/randomizeAndExtendPitchArray.tsx
@@ -1,22 +1,24 @@
-import {Pitch} from "../models/Pitch";
+import {PitchesObject} from "../models/PitchesObject";
 
-export const randomizeAndExtendPitchArray = (pitches: Pitch[], quizLength: number): Pitch[] => {
+export const randomizeAndExtendPitchArray = (pitches: PitchesObject[], quizLength: number): PitchesObject[] => {
     const shallowCopyPitches = pitches.slice();
     const shuffledPitches = shallowCopyPitches.sort((a, b) => 0.5 - Math.random());
 
-    const shuffledAndExpandedArray: Pitch[] = expandPitchArray(shuffledPitches);
+    const shuffledAndExpandedArray: PitchesObject[] = expandPitchArray(shuffledPitches);
 
-
-    const shuffledExpandedAndIdArray = shuffledAndExpandedArray.map((pitch: Pitch) => {
+    const shuffledExpandedAndIdArray = shuffledAndExpandedArray.map((pitch: PitchesObject) => {
         pitch.instanceId = Math.random().toString(36).substring(2,40);
         return pitch;
     })
 
-    return shuffledExpandedAndIdArray.slice(0, quizLength);
+    //Todo: Eliminate repeated notes!
+    const reShuffledExpandedAndIdArray = shuffledExpandedAndIdArray.sort((a, b)=>0.5 - Math.random());
+
+    return reShuffledExpandedAndIdArray.slice(0, quizLength);
 
 }
 
-const expandPitchArray = (array: Pitch[]): Pitch[] => {
+const expandPitchArray = (array: PitchesObject[]): PitchesObject[] => {
     array = array.concat(array);
     array = array.concat(array);
     array = array.concat(array);
