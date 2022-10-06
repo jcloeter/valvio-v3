906be3939b26edbcb96789a9685d2283f9ad2ad3
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-06 08:28:07 -0500

    Add metrics to quiz game

--- app/src/components/ui/MetricsCard.tsx
+++ app/src/components/ui/MetricsCard.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
 import LightGreyCard from "./LightGreyCard";
+import {useAppSelector} from "../../features/hooks";
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import GradingIcon from '@mui/icons-material/Grading';
+import ValvioDirections from "./ValvioDirections";
+import WrongAnswerFeedback from "./WrongAnswerFeedback";
+import CorrectAnswerFeedback from "./CorrectAnswerFeedback";
+import {useSelector} from "react-redux";
+import {RootState} from "../../features/store";
 
 const MetricsCard = () => {
+    const quizSlice = useAppSelector(state => state.quizAttemptSlice);
+    const currentPitchIndex = useSelector<RootState, number>((state) => state.quizAttemptSlice.currentPitchIndex);
+
+
+
+
+    //We need to display:
+    //Level Name and Number
+    //If transposition include transpo instructions
+    //Current Score = inCorrect *
+    //Index+1 / quizLength
+    // Index / startTime - Date.now()
+    // Instructions if index === 0 AND incorrect && correct guesses === 0;
+    //  else: if attempt isIncorrect display X Try Again
+    //  esle: if attempt isCorrect show pitch[index-1]image with (Checkmark) Gorrect! ${pitchname} is ${position}
+
+    let feedback = <ValvioDirections/>;
+    if (quizSlice.isUserCorrect && currentPitchIndex > 0) {
+        feedback = <CorrectAnswerFeedback/>
+    } else if (!quizSlice.isUserCorrect) {
+        feedback = <WrongAnswerFeedback/>
+    }
+
+    let speed: string | number = "n/a";
+    if (currentPitchIndex >0 && quizSlice.startTime) {
+        speed = ((Date.now() - quizSlice.startTime)/(currentPitchIndex*1000)).toFixed(2)+'s/pitch';
+    }
+
     return (
         <LightGreyCard>
-            <h1>MetricsCard!!!</h1>
+            <>
+                <section>
+                    <p> <b>Level {quizSlice.quizLevel}.</b> {quizSlice.quizName}</p>
+                    <p> {quizSlice.isTransposition ?
+                        <><b>Transposition: </b>{quizSlice.transpositionDescription}</> :
+                        <><b>Mode:</b> Pitch Recognition</> }</p>
+                </section>
+                <section>
+                    <div>
+                        <GradingIcon sx={{color : "gray"}}/> {quizSlice.currentScore.toFixed(0)}%
+                    </div>
+                    <div>
+                        <MusicNoteIcon sx={{color : "gray"}}/> {currentPitchIndex}/{quizSlice.quizLength}
+                    </div>
+                    <div>
+                        <AvTimerIcon sx={{color : "gray"}}/> {speed}
+                    </div>
+                </section>
+                <section>
+                    {feedback}
+                </section>
+            </>
         </LightGreyCard>
     );
 };
--- app/src/pages/CompletedQuizSummaryPage.tsx
+++ app/src/pages/CompletedQuizSummaryPage.tsx
@@ -1,20 +1,26 @@
 import React, {useEffect} from 'react';
-import {useAppDispatch} from "../features/hooks";
-import {endCompletedQuiz} from "../features/quizData/quizSlice";
+import {useAppDispatch, useAppSelector} from "../features/hooks";
+import {endCompletedQuiz, resetQuizData} from "../features/quizData/quizSlice";
 import {usePatchQuizAttemptMutation} from "../features/quizData/quiz-api";
 import {useSelector} from "react-redux";
 import {RootState} from "../features/store";
+import Button from "@mui/material/Button";
+import {useNavigate} from 'react-router-dom';
+import GradingIcon from "@mui/icons-material/Grading";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import AvTimerIcon from "@mui/icons-material/AvTimer";
 
 const CompletedQuizSummaryPage = () => {
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
     const [patchQuizAttemptMutation, {data, isError, isLoading}] = usePatchQuizAttemptMutation();
 
     const quizAttemptId = useSelector<RootState, number | null>((state) => state.quizAttemptSlice.quizAttemptId);
     const startTime = useSelector<RootState, number | null>((state) => state.quizAttemptSlice.startTime);
     const endTime = useSelector<RootState, number | null>((state) => state.quizAttemptSlice.endTime);
+    const quizSlice = useAppSelector(state => state.quizAttemptSlice);
+    const currentPitchIndex = useSelector<RootState, number>((state) => state.quizAttemptSlice.currentPitchIndex);
 
-    console.log("STARTTIME: " + startTime);
-    console.log("ENDTIME: " + endTime);
 
     let completedIn: number | null;
     if (startTime && endTime) {
@@ -22,19 +28,46 @@ const CompletedQuizSummaryPage = () => {
     }
 
     useEffect(()=>{
-
         patchQuizAttemptMutation({
             userId: 7,
             quizAttemptId,
             completedIn
         });
-
     }, [])
+
+    const navigateHome = () => {
+        navigate('/');
+    }
+
+    let loadingContent;
+    let errorContent;
+    if (isLoading) {
+         loadingContent = "Sending Score to server- Don't leave this page just yet";
+    }
+    if (isError) {
+        errorContent = "There was a mistake sending your quiz information to the server. Check your internet connection and try again."
+    }
+
+    let speed: string | number = "n/a";
+    if (currentPitchIndex >0 && quizSlice.startTime) {
+        speed = ((Date.now() - quizSlice.startTime)/(currentPitchIndex*1000)).toFixed(2)+'s/pitch';
+    }
 
     return (
         <div>
-            <h1>Congrats on Finishing the Quiz</h1>
-            {isLoading && "Sending Score to server"}
+            <h1>Congrats on Finishing</h1>
+            {loadingContent}
+            {errorContent}
+            <div>
+                <GradingIcon sx={{color : "gray"}}/> {quizSlice.currentScore.toFixed(0)}%
+            </div>
+            <div>
+                <MusicNoteIcon sx={{color : "gray"}}/> Wrong Answers: {quizSlice.incorrectPitchAttempts}/{quizSlice.quizLength}
+            </div>
+            <div>
+                <AvTimerIcon sx={{color : "gray"}}/> {speed}
+            </div>
+            <Button onClick = {navigateHome}>Back to Home</Button>
         </div>
     );
 };
