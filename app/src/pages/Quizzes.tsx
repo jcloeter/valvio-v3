af9fc6d9e2f6fd4ce9a5a4f1f211990806e1f9e9
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-06 08:27:36 -0500

    Add logic to reset quiz data on homepage

--- app/src/pages/Quizzes.tsx
+++ app/src/pages/Quizzes.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import QuizList from "../components/ui/QuizList";
+import {resetQuizData} from "../features/quizData/quizSlice";
+import {useAppDispatch} from "../features/hooks";
 
 const Quizzes = () => {
+    const dispatch = useAppDispatch();
+    dispatch(resetQuizData());
+
     return (
         <div>
             <QuizList/>
