e7753b5174a787e5136a0358b85d0f1bb8c44742
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-06 08:31:03 -0500

    Expand quizSlice definition

--- app/src/features/quizData/quizSlice.tsx
+++ app/src/features/quizData/quizSlice.tsx
@@ -16,12 +16,16 @@ interface QuizSlice {
     extendedPitches: PitchesObject[],
     isTransposition: boolean,
     transpositionInterval: number,
+    transpositionDescription: string | null,
     startTime: number | null,
     endTime: number | null,
     quizStatus:  "exited" | "in progress" | "complete" | "error" | "failed" | "uninitialized" ;
     quizLength: number | null,
     currentPitchIndex: number,
     quizAttemptId: number | null,
+    quizName: string | null,
+    quizLevel: number | null,
+    isUserCorrect: boolean,
 }
 
 const initialState: QuizSlice= {
@@ -34,12 +38,16 @@ const initialState: QuizSlice= {
     extendedPitches: [],
     isTransposition: false,
     transpositionInterval: 0,
+    transpositionDescription: null,
     startTime: null,
     endTime: null,
     quizStatus: "uninitialized",
     quizLength: null,
     currentPitchIndex: 0,
     quizAttemptId: null,
+    quizName:  null,
+    quizLevel: null,
+    isUserCorrect: true,
 };
 
 export const quizSlice = createSlice({
@@ -47,13 +55,18 @@ export const quizSlice = createSlice({
     initialState : initialState,
     reducers: {
         submitWrongAnswer: (state)=>{
-            state.incorrectPitchAttempts += 1
+            state.incorrectPitchAttempts += 1;
+            state.isUserCorrect = false;
+            if (state.quizLength){
+                state.currentScore = (100/state.quizLength)*state.correctPitchAttempts - (state.incorrectPitchAttempts *(100/state.quizLength) * .75)
+            }
         },
         submitCorrectAnswer: (state)=>{
             state.correctPitchAttempts += 1;
             state.currentPitchIndex += 1;
+            state.isUserCorrect = true;
             if (state.quizLength){
-                state.currentScore += 100/(state.quizLength);
+                state.currentScore = (100/state.quizLength)*state.correctPitchAttempts - (state.incorrectPitchAttempts *(100/state.quizLength) * .75)
             }
         },
         setStartTime: (state) =>{
@@ -63,6 +76,28 @@ export const quizSlice = createSlice({
         endCompletedQuiz: (state) => {
             state.quizStatus = "complete";
             state.endTime = Date.now();
+        },
+        resetQuizData: (state) => {
+            // state = initialState;
+            state.quizId= null;
+            state.userid= null;
+            state.currentScore= 0;
+            state.correctPitchAttempts= 0;
+            state.incorrectPitchAttempts= 0;
+            state.uniquePitches= [];
+            state.extendedPitches= [];
+            state.isTransposition= false;
+            state.transpositionInterval= 0;
+            state.transpositionDescription= null;
+            state.startTime= null;
+            state.endTime= null;
+            state.quizStatus= "uninitialized";
+            state.quizLength= null;
+            state.currentPitchIndex= 0;
+            state.quizAttemptId= null;
+            state.quizName=  null;
+            state.quizLevel= null;
+            state.isUserCorrect= true
         },
         endIncompleteQuiz: () =>{},
     },
@@ -74,6 +109,9 @@ export const quizSlice = createSlice({
                 state.isTransposition = action.payload.isTransposition;
                 state.quizId = action.payload.quizId;
                 state.quizLength = action.payload.quizLength;
+                state.transpositionDescription = action.payload.transpositionDescription;
+                state.quizName = action.payload.quizName;
+                state.quizLevel = action.payload.quizLevel;
             })
             .addMatcher(quizApi.endpoints.createQuizAttempt.matchFulfilled, (state, action)=>{
                 state.quizAttemptId = action.payload.id;
@@ -82,4 +120,4 @@ export const quizSlice = createSlice({
 });
 
 export default quizSlice.reducer;
-export const { submitWrongAnswer, submitCorrectAnswer, endCompletedQuiz, setStartTime } = quizSlice.actions;
+export const { submitWrongAnswer, submitCorrectAnswer, endCompletedQuiz, setStartTime, resetQuizData } = quizSlice.actions;
