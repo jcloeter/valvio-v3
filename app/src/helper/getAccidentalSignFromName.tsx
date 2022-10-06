2ba626c968b5c3d649c9ea2cd79c4c89ebad0fc6
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-06 15:56:26 -0500

    Create getAccidentalSignFromName.tsx

---
+++ app/src/helper/getAccidentalSignFromName.tsx
@@ -0,0 +1,17 @@
+export const getAccidentalSignFromName = (name: string) => {
+    if (name === "flat") {
+        return '♭';
+    }
+    if (name === "double-flat") {
+        return '♭♭';
+    }
+    if (name === "sharp") {
+        return '♯';
+    }
+    if (name === "double-sharp") {
+        return '𝄪';
+    }
+    if (name === "natural") {
+        return '♮';
+    }
+};
