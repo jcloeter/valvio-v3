802a60ba485ef1ed91a94b1203cb7ee190c38d9a
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-06 15:54:55 -0500

    Add container components

---
+++ app/src/components/layout/PitchImageMetricsContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./PitchImageMetricsContainer.module.css";
+import {Box} from "@mui/material";
+import Container from "@mui/material/Container";
+
+const PitchImageMetricsContainer: React.FC<{children: JSX.Element}> = (props) => {
+    return (
+        <Container>
+            <Box className = {styles["pitch-container"]}>
+                {props.children}
+            </Box>
+        </Container>
+    );
+};
+
+export default PitchImageMetricsContainer;
---
+++ app/src/components/ui/MetricIcons.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from "@mui/material/Container";
+import styles from "./Metricicons.module.css";
+
+const MetricIcons: React.FC<{children: JSX.Element}> = (props) => {
+    return (
+        <div className = {styles["metric-icons-container"]}>
+            {props.children}
+        </div>
+    );
+};
+
+export default MetricIcons;
