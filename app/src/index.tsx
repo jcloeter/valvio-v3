128bfe5e93aaf6b668aa19a937e53eb2b7b19768
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-07 16:01:07 -0500

    Fixed deprecated import

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./features/store";
 
-const root = ReactDOM.createRoot(
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
