05b50d6742c44fd18967790779bbf8ff7ecf2480
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-07 16:00:14 -0500

    Add a page component for quiz history data

---
+++ app/src/pages/QuizAttemptHistoryPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QuizHistoryTable from "../components/ui/QuizHistoryTable";
+
+const QuizAttemptHistoryPage = () => {
+    return (
+        <div>
+            <QuizHistoryTable/>
+            {/*<QuizDataHistory/>*/}
+        </div>
+    );
+};
+
+export default QuizAttemptHistoryPage;
