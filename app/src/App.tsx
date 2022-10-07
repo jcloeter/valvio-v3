44c585fb4fca1ddd9fd76548473e247a2061a3e6
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-07 16:00:57 -0500

    Wrapped all routes except for the History page in Card component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,7 @@ import Quizzes from "./pages/Quizzes";
 import QuizMode from "./pages/QuizMode";
 import QuizLoadingPage from "./pages/QuizLoadingPage";
 import CompletedQuizSummaryPage from "./pages/CompletedQuizSummaryPage";
+import QuizAttemptHistoryPage from "./pages/QuizAttemptHistoryPage";
 
 
 function App() {
@@ -14,17 +15,18 @@ function App() {
     return (
     <div className="App">
       <ResponsiveAppBar/>
-        <PrimaryCard >
+        {/*<PrimaryCard >*/}
             <Routes>
-                <Route path={"/login"} element={<h1>Login</h1>}/>
-                <Route path={"/"} element={<Quizzes/>}/>
-                <Route path={`/loading-quiz/:quizId`} element={<QuizLoadingPage/>}/>
-                <Route path={`/quiz/:quizId`} element={<QuizMode/>}/>
-                <Route path={`/completed-quiz-summary`} element={<CompletedQuizSummaryPage/>}/>
-                <Route path={"/logout"} element={<h1>LOGOUT</h1>}/>
-                <Route path="*" element={<h1>Page Not Found</h1>} />
+                <Route path={"/login"} element={<PrimaryCard><h1>Login</h1></PrimaryCard>}/>
+                <Route path={"/"} element={<PrimaryCard><Quizzes/></PrimaryCard>}/>
+                <Route path={"/history"} element={<QuizAttemptHistoryPage/>}/>
+                <Route path={`/loading-quiz/:quizId`} element={<PrimaryCard><QuizLoadingPage/></PrimaryCard>}/>
+                <Route path={`/quiz/:quizId`} element={<PrimaryCard><QuizMode/></PrimaryCard>}/>
+                <Route path={`/completed-quiz-summary`} element={<PrimaryCard><CompletedQuizSummaryPage/></PrimaryCard>}/>
+                <Route path={"/logout"} element={<PrimaryCard><h1>LOGOUT</h1></PrimaryCard>}/>
+                <Route path="*" element={<PrimaryCard><h1>Page Not Found</h1></PrimaryCard>} />
             </Routes>
-        </PrimaryCard>
+        {/*</PrimaryCard>*/}
     </div>
   );
 }
