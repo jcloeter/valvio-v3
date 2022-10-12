9619834a6e3d8f5718949e9bbd76177a627042bd
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-12 15:13:28 -0500

    Add a logout button to profile page

--- app/src/pages/ProfilePage.tsx
+++ app/src/pages/ProfilePage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PrimaryCard from "../components/layout/PrimaryCard";
+import Button from "@mui/material/Button";
+import {getAuth, signOut} from 'firebase/auth';
 
 const ProfilePage = () => {
+
+    const auth = getAuth();
+    const handleLogOut = async ()=>{
+        console.log("singing out from firebase now")
+        await signOut(auth);
+    }
+
     return (
         <PrimaryCard>
             <h1>Welcome User!</h1>
+            <Button onClick={handleLogOut}>Logout</Button>
         </PrimaryCard>
     );
 };
