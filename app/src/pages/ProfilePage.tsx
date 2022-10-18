f194661397c27b6c1304b58f171816a5f876402b
jcloeter-kipsu <95771672+jcloeter-kipsu@users.noreply.github.com>
2022-10-18 15:30:31 -0500

    Added image to profile page

--- app/src/pages/ProfilePage.tsx
+++ app/src/pages/ProfilePage.tsx
@@ -5,6 +5,7 @@ import {getAuth, signOut} from 'firebase/auth';
 import {useSelector} from "react-redux";
 import {User} from "../models/User";
 import {RootState} from "../features/store";
+import {Avatar} from "@mui/material";
 
 const ProfilePage = () => {
     const authSlice = useSelector((state: RootState) => state.authSlice);
@@ -17,9 +18,15 @@ const ProfilePage = () => {
 
     return (
         <PrimaryCard>
-            <h1>Welcome User!</h1>
-            <p>According to redux you are {authSlice.isAuthenticated ? "signed in" : "signed out"}</p>
-            <p>Username: {authSlice.displayName}</p>
+            <h1>Welcome {authSlice.displayName}!</h1>
+            {authSlice.photoUrl && <img alt="user_avatar" style = {{maxHeight: '100px', borderRadius: '50%' }} src={authSlice.photoUrl || ''}/>}
+            {/*<img alt="user_avatar" style = {{maxHeight: '100px', borderRadius: '50%' }} src={authSlice.photoUrl || ''}/>*/}
+            {/*<Avatar alt="user_avatar" src={authSlice.photoUrl || ''} sx={{ width: 100, height: 100 }}/>*/}
+            {/*<p>According to redux you are {authSlice.isAuthenticated ? "signed in" : "signed out"}</p>*/}
+            <br/>
+            <p>Username: {(authSlice.displayName || "anonymous")}</p>
+            <br/>
+            <p>Email: {(authSlice.email) || "no email"}</p>
             <Button variant = "contained" onClick={handleLogOut}>Logout</Button>
         </PrimaryCard>
     );
